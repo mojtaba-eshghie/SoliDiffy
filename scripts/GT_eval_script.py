dce9350bca4435c139da1d2ca20ff443981f4d0c
ViktorAryd <viktoraaryd@gmail.com>
2024-02-22 09:45:50 +0100

    Logging and doc change to script

--- scripts/GT_eval_script.py
+++ scripts/GT_eval_script.py
@@ -1,14 +1,11 @@
-#Run gumtree to get data on diffs, split into contract and number of mutations.
-#Store results in format that can be accessed later for analysis and figures. 
-
-#Json/XML? Python 2d matrix of lists to file? 
-
+#Runs gumtree to get data on diffs, puts result in 2d matrix with axes contract and number of mutations.
+#Stores results in pickle file. 
 
 # -----------Gumtree Results----------------
 # Contracts |      Diff Results/# Mutations
 # ------------------------------------------
 # Contract1 | 1 mutation, 2 mutations, ...
-# Contract2 | ...
+# Contract2 | 1 mutation, ...
 # ...
 # Contractn | ...
 # ------------------------------------------
@@ -19,8 +16,8 @@ import os
 import xml.etree.ElementTree as ET
 import subprocess
 import pickle
+import time
 
-logging = True
 
 def parse_input():
     if len(sys.argv) != 3:
@@ -32,6 +29,8 @@ def parse_input():
 
 #Uses gumtree to get the diff data of two files
 def get_GT_diff_data(filepath1, filepath2):
+    save_full_diff = True
+
     diff = subprocess.check_output('gumtree textdiff -f XML ' +  filepath1 + " " + filepath2, shell=True).decode()
     
     #wrap result to get single XML root and convert to tree
@@ -42,7 +41,12 @@ def get_GT_diff_data(filepath1, filepath2):
     #Get number of edit actions and matches from tree
     n_edits = len(tree.findall('actions')[0])
     n_matches = len(tree.findall('matches')[0])
-    return (n_edits, n_matches)
+    
+    #Append full diff to res if flag is set and return res
+    res = [n_edits, n_matches]
+    if save_full_diff:
+        res.append(diff)
+    return res
 
 #Gets the GT diffs between all mutants of a contract in a directory
 def get_contract_diffs(contract_path, contract):
@@ -50,9 +54,8 @@ def get_contract_diffs(contract_path, contract):
     unmutated = contract_path + "/" + "original/" + contract + ".sol"
     num_contracts = len(os.listdir(contract_path))
 
-    for i in range(1, num_contracts):
-        if(logging):
-            print('Calculating diff ' + str(i) + '/' + str(num_contracts - 1), end='\r')
+    for i in range(1, num_contracts):    
+        print('Calculating diff ' + str(i) + '/' + str(num_contracts - 1), end='\r')
 
         diff = get_GT_diff_data(unmutated, contract_path + "/" + str(i) + "/" + contract + ".sol")
         res.append(diff)
@@ -63,32 +66,32 @@ def get_contract_diffs(contract_path, contract):
 def calculate_diffs(contracts_path):
     contracts = os.listdir(contracts_path)
     res = []
-
     for c in contracts:
-        if(logging):
-            print("Calculating diffs for " + c + "...")
+        seconds = int(time.time())
+        print("Calculating diffs for " + c + "...")
 
         res.append(get_contract_diffs(contracts_path + c, c))
 
-        if(logging): 
-            print("===========================")
+        m, s = divmod(int(time.time()) -  seconds, 60)
+        print("Diffs calculated in: " + str(m) + ":" + str(s))
+        print("======================================")
+
     return res
 
 #Saves results as a python object in a results file
 def save_res_to_file(results):
     out = output_path + "results.pickle"
-
-    if(logging):
-        print("Results: " + results)
-        print("Dumping results to " + out)
-    
+    print("\nDumping results to " + out)
     out_file = open(out, "wb")
     pickle.dump(res, out_file)
 
 if __name__ == "__main__":
+    seconds = int(time.time())
+    
     contracts_path, output_path = parse_input()
     res = calculate_diffs(contracts_path)
     save_res_to_file(res)
-   
-
+    
+    m, s = divmod(int(time.time()) -  seconds, 60)
+    print("Generated " + str(len(res)*(len(res[0]) - 1)) + " diffs in " + str(m) + "m:" + str(s) + "s")
     
