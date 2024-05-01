66068eacb7700d9abd3e2f57c3c3e337b0d39687
varyd <varyd@kth.se>
2024-05-01 11:50:14 +0200

    refactor result analysis

--- scripts/results/res_analysis.py
+++ scripts/results/res_analysis.py
@@ -4,142 +4,121 @@ import matplotlib.pyplot as plt
 import numpy
 
 
-difft_file = open("results-difft.pickle", "rb")
-gt_file = open("results-GT.pickle", "rb")
+def load_pickles():
+    difft_file = open("results-difft.pickle", "rb")
+    gt_file = open("results-GT.pickle", "rb")
 
-difft = pickle.load(difft_file)
-GT = pickle.load(gt_file)
-num_mut = 10
+    difft = pickle.load(difft_file)
+    GT = pickle.load(gt_file)
 
-difft_res = [0] * num_mut
-difft_count = [0] * num_mut
-difft_mut_res = {} 
-difft_mut_res_count = {}
-
-GT_res = [0] * num_mut
-GT_count = [0] * num_mut
-GT_mut_res = {}
-GT_mut_res_count = {}
+    return {"GT": GT, "difft": difft}
 
 
-"VUR" 
-"OLFD" 
-"CCD" 
-"ORFD" 
+def setup(n_mut):
+    res = [0] * n_mut
+    count = [0] * n_mut
+    mut_res = {}
+    mut_count = {}
 
-"CSC" 
-"CCD" 
-"GVR"
-"ICM" 
+    return {"res": res, "count": count, "mut_res": mut_res, "mut_count": mut_count}
 
 
-#old_removed = ["ICM", "ORFD", "CCD", "OLFD", "LSC","GVR"]
-removed = ["VUR", "OLFD", "CCD", "ORFD", "CSC", "CCD", "GVR","ICM"]
-for contract in difft:
-    for i in range(len(difft[contract])):
-        for r in removed:
-            if r in difft[contract][i].keys():
-                difft[contract][i].pop(r)
-
-for contract in GT:
-    for i in range(len(GT[contract])):
-        for r in removed:
-            if r in GT[contract][i].keys():
-                GT[contract][i].pop(r)
+def remove_mut_operators(diff_results, operators):
+    for diff_tool in diff_results:
+        for contract in diff_results[diff_tool]:
+            for i in range(len(diff_results[diff_tool][contract])):
+                for r in operators:
+                    if r in diff_results[diff_tool][contract][i].keys():
+                        diff_results[diff_tool][contract][i].pop(r)
 
 
-for contract in difft:
-    #mut_in = False
-    #if "CCD" in difft[contract][0].keys():
-    #    print("Contract " + contract)
-    #    mut_in = True
-    for i in range(len(difft[contract])):
-        for mut in difft[contract][i]:
-            difft_count[i] += 1
-            difft_res[i] += difft[contract][i][mut]
+def analyze_diffs(diffs, res_dict, n_mut):
+    is_GT = isinstance(next(iter(next(iter(diffs.values()))[0].values())), list)
 
-            #if mut == "CCD":
-            #    print("mutation #" + str(i+1))
-            #    print("Number of edit actions:", difft[contract][i][mut])
-                
+    for contract in diffs:
+        for i in range(len(diffs[contract])):
+            for mut in diffs[contract][i]:
+                if not diffs[contract][i][mut] == []:
+                    res_dict["count"][i] += 1
+                    if is_GT:
+                        res_dict["res"][i] += diffs[contract][i][mut][0]
+                    else:
+                        res_dict["res"][i] += diffs[contract][i][mut]
+                    
+                    if not mut in res_dict["mut_res"].keys():
+                        res_dict["mut_res"][mut] = [0] * n_mut
+                        res_dict["mut_count"][mut] = [0] * n_mut 
 
-            if not mut in difft_mut_res.keys():
-                difft_mut_res[mut] = [0] * num_mut
-                difft_mut_res_count[mut] = [0] * num_mut
-            difft_mut_res[mut][i] += difft[contract][i][mut]
-            difft_mut_res_count[mut][i] += 1    
-    #if mut_in:
-    #    print("===========================================")
+                    if is_GT:
+                        res_dict["mut_res"][mut][i] += diffs[contract][i][mut][0]
+                    else:
+                        res_dict["mut_res"][mut][i] += diffs[contract][i][mut] 
+                    res_dict["mut_count"][mut][i] += 1    
 
-difft_res = [i / j for i, j in zip(difft_res, difft_count)]
+    #Calculate average results 
+    res_dict["res"] = [i / j for i, j in zip( res_dict["res"],  res_dict["count"])]
 
-for mut in difft_mut_res:
-    for i in range(len(numpy.trim_zeros(difft_mut_res[mut]))):
-        difft_mut_res[mut] = numpy.trim_zeros(difft_mut_res[mut])
-        difft_mut_res_count[mut] = numpy.trim_zeros(difft_mut_res_count[mut])
-        difft_mut_res[mut][i] /= difft_mut_res_count[mut][i]
-
-for contract in GT:
-    for i in range(len(GT[contract])):
-        for mut in GT[contract][i]:
-            if GT[contract][i][mut]:
-                GT_count[i] += 1
-                GT_res[i] += GT[contract][i][mut][0]
-                
-                if not mut in GT_mut_res.keys():
-                    GT_mut_res[mut] = [0] * num_mut
-                    GT_mut_res_count[mut] = [0] * num_mut
-                GT_mut_res[mut][i] += GT[contract][i][mut][0]
-                GT_mut_res_count[mut][i] += 1
-
-GT_res = [i / j for i, j in zip(GT_res, GT_count)]  
-
-for mut in GT_mut_res:
-    for i in range(len(numpy.trim_zeros(GT_mut_res[mut]))):
-        GT_mut_res[mut] = numpy.trim_zeros(GT_mut_res[mut])
-        GT_mut_res_count[mut] = numpy.trim_zeros(GT_mut_res_count[mut])
-        GT_mut_res[mut][i] /= GT_mut_res_count[mut][i]
-
-print("====================================")
-print("difftastic average: " + str(difft_res))
-print("Gumtree average: " + str(GT_res))
-print("successful # of mutations: ", difft_count)
-print("====================================")
+    for mut in res_dict["mut_res"]:
+        for i in range(len(numpy.trim_zeros( res_dict["mut_res"][mut]))):
+            res_dict["mut_res"][mut] = numpy.trim_zeros( res_dict["mut_res"][mut])
+            res_dict["mut_count"][mut] = numpy.trim_zeros( res_dict["mut_count"][mut])
+            res_dict["mut_res"][mut][i] /= res_dict["mut_count"][mut][i]
 
 
-
-print("GUMTREE RESULTS BY MUTATION: ")
-GT_mut_sorted = sorted(GT_mut_res.items(), key = lambda x: x[1][len(x[1])-1])
-for mut in GT_mut_sorted:
-    print(mut[0], mut[1], " counts:", GT_mut_res_count[mut[0]])
-#pprint.pprint(GT_mut_sorted)
-
-print("====================================")
-print("DIFFTASTIC RESULTS BY MUTATION: ")
-difft_mut_sorted = sorted(difft_mut_res.items(), key = lambda x: x[1][len(x[1])-1])
-for mut in difft_mut_sorted:
-    print(mut[0], mut[1], " counts:", difft_mut_res_count[mut[0]])
-
-#pprint.pprint(difft_mut_sorted)
+def print_summary(GT, difft):
+    print("====================================")
+    print("difftastic average: " + str(difft["res"]))
+    print("successful # of mutations: ", difft["count"])
+    print("Gumtree average: " + str(GT["res"]))
+    print("successful # of mutations: ", GT["count"])
+    print("====================================")
 
 
-
-plt.plot(range(10) ,difft_res, label=("difft_avg"), color="blue")
-for mut in difft_mut_res:
-    x = [i+0.985 for i in range(len(difft_mut_res[mut]))]
-    plt.scatter(x, difft_mut_res[mut], color="blue", s=6)
-
-plt.plot(range(10), GT_res, label ="GT_avg", color = "red")
-for mut in GT_mut_res:
-    x = [i+1.015 for i in range(len(GT_mut_res[mut]))]
-    plt.scatter(x, GT_mut_res[mut], color="red", s=6)
+def print_by_mutation(res_dict):
+    
+    print("EDIT SCRIPT LENGTH BY MUTATION:")
+    print("=====================================================")
+    mut_sorted = sorted(res_dict["mut_res"].items(), key = lambda x: x[1][len(x[1])-1])
+    for mut in mut_sorted:
+        print(mut[0], mut[1], " counts:", res_dict["mut_res"][mut[0]])
+    print("=====================================================")
 
 
-plt.title("Results")
-plt.ylabel("edit actions")
-plt.xlabel("# of mutations")
-plt.minorticks_on()
-plt.xticks(range(11))
-#plt.yticks([0,5,10,15])
-plt.legend()
-plt.show()
+def scatter_with_avg_plot(GT, difft):
+    plt.plot(range(10) ,difft["res"], label=("difft_avg"), color="blue")
+    for mut in difft["mut_res"]:
+        x = [i+0.985 for i in range(len(difft["mut_res"][mut]))]
+        plt.scatter(x, difft["mut_res"][mut], color="blue", s=6)
+
+    plt.plot(range(10), GT["res"], label ="GT_avg", color = "red")
+    for mut in GT["mut_res"]:
+        x = [i+1.015 for i in range(len(GT["mut_res"][mut]))]
+        plt.scatter(x, GT["mut_res"][mut], color="red", s=6)
+
+    plt.title("Results")
+    plt.ylabel("edit actions")
+    plt.xlabel("# of mutations")
+    plt.minorticks_on()
+    plt.xticks(range(11))
+    plt.legend()
+    plt.show()
+
+
+if __name__ ==  '__main__':
+    num_mut = 10
+    pickles = load_pickles()
+    #outliers = ["ORFD", "CBD", "OLFD", "CCD", "EHC", "EED", "CSC", "RSD", "ACM", "LSC", "ICM"]
+    #remove_mut_operators(pickles, outliers)
+
+    GT_res_dict = setup(num_mut)
+    analyze_diffs(pickles["GT"], GT_res_dict, num_mut)
+
+    difft_res_dict = setup(num_mut)
+    analyze_diffs(pickles["difft"], difft_res_dict, num_mut)
+
+    #print_summary(GT_res_dict, difft_res_dict)
+
+    #print_by_mutation(difft_res_dict)
+    print_by_mutation(GT_res_dict)
+
+    scatter_with_avg_plot(GT_res_dict, difft_res_dict)
